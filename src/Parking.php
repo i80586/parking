e171d1057b0006b2ecd28e21e3aa27722021b8b0
i80586 <rasim.ashurov@gmail.com>
2018-01-02 18:47:40 +0400

    realized entrace and departure

--- src/Parking.php
+++ src/Parking.php
@@ -2,34 +2,47 @@
 
 namespace parking;
 
+use parking\spaces\ParkingSpace;
+
 /**
  * Parking class
  */
 class Parking
 {
     
-    private $parkingSpaces = [];
+    private $parkingSpaces = [
+        25 => [
+            'size' => 2
+        ],
+        35 => [
+            'size' => 1
+        ],
+        106 => [
+            'size' => 1
+        ]
+    ];
     
     public function entrance(cars\Car $car): Ticket
-    {
-        $ticket = new Ticket(25);
+    {        
+        foreach ($this->parkingSpaces as $identifier => $space) {
+            if (empty($space['took']) && $car->getSpaceSize() <= $space['size']) {
+                $ticket = new Ticket($identifier);
+                $this->parkingSpaces[$identifier]['took'] = new ParkingSpace($car, $ticket);
+                
+                return $ticket;
+            }
+        }
         
-        $parkingSpace = ($car->getSpacesCount() > 1) 
-                        ? new spaces\FreighCarSpace($car, $ticket)
-                        : new spaces\PassengerCarSpace($car, $ticket);
-        
-        $this->parkingSpaces[$ticket->getIdentifier()] = $parkingSpace;
-        
-        return $ticket;
+        throw new NoEmptySpaceException();
     }
     
-    public function departure(integer $spaceNumber)
+    public function departure(Ticket $ticket)
     {
-        if (!isset($this->parkingSpaces[$spaceNumber])) {
+        if (!isset($this->parkingSpaces[$ticket->getIdentifier()])) {
             throw new SpaceNotFoundException();
         }
         
-        unset($this->parkingSpaces[$spaceNumber]);
+        unset($this->parkingSpaces[$ticket->getIdentifier()]['took']);
     }
     
 }
